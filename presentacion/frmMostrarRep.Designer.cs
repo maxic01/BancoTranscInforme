0435aa0b6c28ab19a788dc61ea14130b690356d9
maxic01 <maxi1998calvo@gmail.com>
2022-09-04 12:34:19 -0300

    Añadi un reporte separado por grupos de tipos de cuenta

--- presentacion/frmMostrarRep.Designer.cs
+++ presentacion/frmMostrarRep.Designer.cs
@@ -30,35 +30,36 @@
         {
             this.components = new System.ComponentModel.Container();
             Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
-            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
-            this.bancoDataSet = new BancoTransacc.bancoDataSet();
             this.saldosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.bancoDataSet = new BancoTransacc.bancoDataSet();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
             this.saldosTableAdapter = new BancoTransacc.bancoDataSetTableAdapters.SaldosTableAdapter();
-            ((System.ComponentModel.ISupportInitialize)(this.bancoDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.saldosBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bancoDataSet)).BeginInit();
             this.SuspendLayout();
             // 
-            // reportViewer1
+            // saldosBindingSource
             // 
-            reportDataSource1.Name = "DataSet1";
-            reportDataSource1.Value = this.saldosBindingSource;
-            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
-            this.reportViewer1.LocalReport.ReportEmbeddedResource = "BancoTransacc.presentacion.Report1.rdlc";
-            this.reportViewer1.Location = new System.Drawing.Point(12, 12);
-            this.reportViewer1.Name = "reportViewer1";
-            this.reportViewer1.ServerReport.BearerToken = null;
-            this.reportViewer1.Size = new System.Drawing.Size(776, 426);
-            this.reportViewer1.TabIndex = 0;
+            this.saldosBindingSource.DataMember = "Saldos";
+            this.saldosBindingSource.DataSource = this.bancoDataSet;
             // 
             // bancoDataSet
             // 
             this.bancoDataSet.DataSetName = "bancoDataSet";
             this.bancoDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
-            // saldosBindingSource
+            // reportViewer1
             // 
-            this.saldosBindingSource.DataMember = "Saldos";
-            this.saldosBindingSource.DataSource = this.bancoDataSet;
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.saldosBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "BancoTransacc.presentacion.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(800, 450);
+            this.reportViewer1.TabIndex = 0;
             // 
             // saldosTableAdapter
             // 
@@ -74,8 +75,8 @@
             this.Text = "frmMostrarRep";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.frmMostrarRep_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.bancoDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.saldosBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bancoDataSet)).EndInit();
             this.ResumeLayout(false);
 
         }
